c11aed0e4045b821ef6a0bb578a044e51a1e85c0
mturilli <matteo.turilli@gmail.com>
2019-10-10 01:38:07 +0200

    Script for heterogeneous runs on Summit, Stampede2 and Frontera

---
+++ setup.sh
@@ -0,0 +1,8 @@
+. ~/Virtualenvs/stampede2/bin/activate
+export RADICAL_VERBOSE="DEBUG"
+export RADICAL_PILOT_DBURL="mongodb://mturilli:m7ur1ll1@one.radical-project.org:27017/mturilli"
+export RADICAL_LOG_LVL="DEBUG"
+export RADICAL_LOG_TGT="radical.log"
+export RADICAL_PROFILE="TRUE"
+export RMQ_HOSTNAME="two.radical-project.org"
+export RMQ_PORT="33235"
